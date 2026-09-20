f665bf82ebea9565d11ab39cce9da52ab11850ba
IVAN-GUY <kaluginivan2002@mail.ru>
2026-09-20 06:17:04 +0400

    added task

---
+++ Simple_Fun_38_House_Of_Cats.py
@@ -0,0 +1,33 @@
+# Task
+# There are some people and cats in a house. You are given the number of legs they have all together. Your task is to return an array containing every possible number of people that could be in the house sorted in ascending order. It's guaranteed that each person has 2 legs and each cat has 4 legs.
+
+# Example
+# For legs = 6, the output should be [1, 3].
+
+# There could be either 1 cat and 1 person (4 + 2 = 6) or 3 people (2 * 3 = 6).
+
+# For legs = 2, the output should be [1].
+
+# There can be only 1 person.
+
+# Input/Output
+# [input] integer legs
+
+# The total number of legs in the house.
+
+# Constraints: 2 ≤ legs ≤ 100.
+
+# [output] an integer array
+
+# Every possible number of people that can be in the house.
+
+def house_of_cats(legs):
+    if legs % 2 != 0:
+        return []
+    
+    result = []
+    for people in range(legs // 2 + 1):
+        if (legs - 2 * people) % 4 == 0:
+            result.append(people)
+    
+    return result
